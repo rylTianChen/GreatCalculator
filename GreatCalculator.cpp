f6c7b8c4b6533351bb3f38be2531b23e09442428
rylTianChen <lyrTianChen@outlook.com>
2026-04-25 09:30:48 +0800

    Update GreatCalculator.cpp

--- GreatCalculator.cpp
+++ GreatCalculator.cpp
@@ -9,40 +9,43 @@ const char version[] = "1.5";
 using namespace grnum;
 
 short math_check(vi &a, vi &b, char op){
-    if(op=='+' || op=='-' || op=='*') return 0;//±Ø¶¨ÄÜËã
-    if(op == '^'){//ÃİÔËËãÅĞ¶Ï
-        if(b[0] < 0) return 2;//¸º´ÎÃİ
+    if(op=='+' || op=='-' || op=='*') return 0;//å¿…å®šèƒ½ç®—
+    if(op == '^'){//å¹‚è¿ç®—åˆ¤æ–­
+        if(b[0] < 0) return 2;//è´Ÿæ¬¡å¹‚
         if(is_ZERO(a) && is_ZERO(b)) return 3;//0^0
-        if(is_ZERO(b)) return -2;
-        if(is_ZERO(a)) return -1;//½á¹ûÎª0
-        if(a[0]==1 && a[1]==1) return -2;//½á¹ûÎª1
         if(a[0]==-1 && a[1]==1) return (b[1]&1) ? -3 : -2;
-        if(abs(a[0])>1 || a[1]>1){//·Ç0 +1 -1
-            if(b[0] > 8) return 5;//½á¹ûÌ«´ó
+        if(abs(a[0])>1 || a[1]>1){//é0 +1 -1
+            if(b[0] > 8) return 5;//ç»“æœå¤ªå¤§
         }
     }
-    //²»ÄÜ³ıÒÔ0¡¢Ä£0
+    //ä¸èƒ½é™¤ä»¥0ã€æ¨¡0
     if(op=='/' && abs(b[0])==1 && b[1]==0) return 1;
     if(op=='%' && abs(b[0])==1 && b[1]==0) return 4;
     return 0;
 }
 
 int main(){
+	#ifdef _WIN32
 	enable_ansi_support();
+	#endif
     int ret, lang; char op;
     vi a(1, 0), b(1, 0), c(1, 0), d(1, 0);
-    HP C;
-    lang = lan_ch(); instructions(lang); init();
+    #ifndef ANDR15 //To mark my Android phone
+    lang = lan_ch();
+    #else
+ 	lang = 2; //Use English in Android Termux
+	#endif
+	instructions(lang); init();
  
     while(1){
         endline();
         ret = read(a, b, c, d, op);
         if(ret){
-        	printf(COLOR_RED);
+        	printf(COLOR_ERR);
             if(lang == 1){
-                if(ret == 1) puts("ÊäÈë²»ÍêÕû");
-                else if(ret == 2) puts("ÊäÈë¶àÓàÄÚÈİ");
-                else if(ret == 3) puts("Î´Öª×Ö·û");
+                if(ret == 1) puts("è¾“å…¥ä¸å®Œæ•´");
+                else if(ret == 2) puts("è¾“å…¥å¤šä½™å†…å®¹");
+                else if(ret == 3) puts("æœªçŸ¥å­—ç¬¦");
             }else if(lang == 2){
                 if(ret == 1) puts("Incomplete input");
                 else if(ret == 2) puts("Redundant content");
@@ -53,13 +56,13 @@ int main(){
         }
         ret = math_check(a, b, op);
         if(ret > 0){
-        	printf(COLOR_RED);
+        	printf(COLOR_ERR);
             if(lang == 1){
-                if(ret == 1) puts("²»ÄÜ³ıÒÔ0");
-                if(ret == 2) puts("Ôİ²»Ö§³Ö¸º´ÎÃİ");
-                if(ret == 3) puts("0^0ÎŞÒâÒå");
-                if(ret == 4) puts("²»ÄÜÄ£0"); 
-                if(ret == 5) puts("½á¹û¹ı´ó");
+                if(ret == 1) puts("ä¸èƒ½é™¤ä»¥0");
+                if(ret == 2) puts("æš‚ä¸æ”¯æŒè´Ÿæ¬¡å¹‚");
+                if(ret == 3) puts("0^0æ— æ„ä¹‰");
+                if(ret == 4) puts("ä¸èƒ½æ¨¡0"); 
+                if(ret == 5) puts("ç»“æœè¿‡å¤§");
             }else if(lang == 2){
                 if(ret == 1) puts("Cannot divide by zero");
                 if(ret == 2) puts("Negative exponents not supported");
@@ -71,25 +74,25 @@ int main(){
             continue;
         }
         d = c;
-        if(ret < 0){
-            if(ret == -1) c = ZERO;
-            if(ret == -2) c = ONE;
-            if(ret == -3) c = M_ONE;
-        }else{
-            HP A(zip(a)), B(zip(b));
-            if(op == '+') C = A + B;
-            if(op == '-') C = A - B;
-            if(op == '*') C = A * B;
-            if(op == '/') C = A / B;
-            if(op == '^') C = HP_pow(A, B);
-            if(op == '%') C = A % B;
-            if(op == '&') C = A & B;
-            if(op == '|') C = A | B;
-        }
+	    HP A(zip(a)), B(zip(b)), C;
+	    //åœ¨äº†è§£å­˜å‚¨é€»è¾‘ä¹‹å‰, ä¸è¦ä½¿ç”¨è¿™ä¸ªæ„é€ å‡½æ•°
+	    //Befor you learn about storage method, do not use this constructor
+	    if(op == '+') C = A + B;
+	    if(op == '-') C = A - B;
+	    if(op == '*') C = A * B;
+	    if(op == '/') C = A / B;
+	    if(op == '^') C = HP_pow(A, B);
+	    if(op == '%') C = A % B;
+	    if(op == '&') C = A & B;
+	    if(op == '|') C = A | B;
         if(!C.isEMPTY()){
         	c = C.vec();
+        	#ifndef ANDR15
             out(c, 1), sci_out(c, lang, 1);//log
-            out(c, 0), ans_out(c), sci_out(c, lang, 0);//¸øÓÃ»§ 
+            out(c, 0), ans_out(c), sci_out(c, lang, 0);//ç»™ç”¨æˆ·
+            #else
+            out(c, 0), sci_out(c, lang, 0);
+            #endif
         }
     }
     return 0;//Maybe I don't have to write this...
