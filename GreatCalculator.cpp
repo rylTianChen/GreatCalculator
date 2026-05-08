1088a712621d1a28d349971ea0e0d24435f18643
rylTianChen <lyrTianChen@outlook.com>
2026-05-08 17:01:12 +0800

    Update GreatCalculator.cpp

--- GreatCalculator.cpp
+++ GreatCalculator.cpp
@@ -5,22 +5,18 @@
 #include"errs.h"
 #include"init_func.h"
 #include"hpcalc.h"
-using namespace grnum;
 
 int main(){
-	#ifdef _WIN32
-		enable_ansi_support();
-	#endif
-    int ret, lang; char op;
-    vi a(1, 0), b(1, 0), c(1, 0), d(1, 0);
-    #ifndef ANDR15
-    	lang = lan_ch();
-    #else
- 		lang = 2;
-	#endif
-	instructions(lang);
-	init();
- 
+    enable_ansi_support();
+    int ret;
+    char op;
+    vi a(1, 0), b(1, 0);
+    vi c(1, 0), d(1, 0);
+    int lang = lan_ch();
+
+    instructions(lang);
+    init();
+
     while(1){
         endline();
         ret = read(a, b, c, d, op);
@@ -30,28 +26,30 @@ int main(){
         }
         ret = math_check(a, b, op);
         if(ret > 0){
-        	error_output(lang, ret);
+            error_output(lang, ret);
             continue;
         }
         d = c;
-	    HP A(zip(a)), B(zip(b)), C;
-	    //在了解存储逻辑之前, 不要使用这个构造函数
-	    if(op == '+') C = A + B;
-	    if(op == '-') C = A - B;
-	    if(op == '*') C = A * B;
-	    if(op == '/') C = A / B;
-	    if(op == '^') C = HP_pow(A, B);
-	    if(op == '%') C = A % B;
-	    if(op == '&') C = A & B;
-	    if(op == '|') C = A | B;
+        grnum::HP A(zip(a)), B(zip(b)), C;
+        //在了解存储逻辑之前, 不要使用这个构造函数
+        if(op == '+') C = A + B;
+        if(op == '-') C = A - B;
+        if(op == '*') C = A * B;
+        if(op == '/') C = A / B;
+        if(op == '^') C = HP_pow(A, B);
+        if(op == '%') C = A % B;
+        if(op == '&') C = A & B;
+        if(op == '|') C = A | B;
+        if(op == '!') C = A ^ B;
         if(!C.isEMPTY()){
-        	c = C.vec();
-        	#ifndef ANDR15
-            out(c, 1), sci_out(c, lang, 1);//log
-            out(c, 0), ans_out(c), sci_out(c, lang, 0);//给用户
-            #else
-            out(c, 0), sci_out(c, lang, 0);
-            #endif
+            c = C.getvec();
+            c = unzip(c);
+	        #ifndef NOFILE
+	            out(c, 1), sci_out(c, lang, 1);//log
+	            out(c, 0), ans_out(c), sci_out(c, lang, 0);//给用户
+	        #else
+	            out(c, 0), sci_out(c, lang, 0);
+	        #endif
         }
     }
     return 0;//Maybe I don't have to write this...
