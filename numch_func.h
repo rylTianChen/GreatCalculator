0e4aad093c0d38e8523858e0b89208e535ac5d65
rylTianChen <lyrTianChen@outlook.com>
2026-05-08 17:11:58 +0800

    Update numch_func.h

--- numch_func.h
+++ numch_func.h
@@ -1,15 +1,13 @@
 #ifndef NUMCH_GR
 #define NUMCH_GR 1
- 
-char fh[] = "*/^%&|";
+
+char fh[] = "*/^%&|!";
 //数相关函数
-namespace my{
-int abs(int x) {return x>0 ? x : -x;}
-}
+#include<cmath> //abs()
 int max(int x, int y) {return x>y ? x : y;}
 int min(int x, int y) {return x<y ? x : y;}
 void swap(int &x, int &y) {int t = x; x = y, y = t;}
-//符号相关函数 
+//字符相关函数
 char posi_nega(char x) {return x=='+' ? '-' : '+';}
 char sym_multi(char x, char y) {return x==y ? '+' : '-';}
 char is_zf(char t) {return t=='+' || t=='-';}
@@ -18,8 +16,8 @@ char is_fh(char x){
         if(ch == x) return 1;
     return 0;
 }
-//数和符号转换函数 
+//数和符号转换函数
 char int_zf(int x) {return x>=0 ? '+' : '-';}
 int zf_int(char x) {return x=='+' ? 1 : -1; }
- 
+
 #endif
